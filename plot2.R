4ce8b39a1a9f23191b31a1c40123775b2c927e70
Prashant Kamsu <prashant.kamsu539@gmail.com>
2014-07-14 03:34:14 +0530

    updated code and png files

---
+++ plot2.R
@@ -0,0 +1,17 @@
+##Reading data
+data <- read.csv("household_power_consumption.txt",header=T, sep=';',na.strings="?",nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+
+##Subsetting required data
+data <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+##Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+##Plotting graph
+plot(data$Global_active_power~data$Datetime, type="l",ylab="Global Active Power (kilowatts)", xlab="")
+
+##saving the plot in "png" format with required pixels
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
